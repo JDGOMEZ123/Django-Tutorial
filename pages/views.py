a2712db3a6361d3d1326a294ddd745dec8d4ff2f
JDGOMEZ123 <jdgomeza3@eafit.edu.co>
2025-08-13 07:40:20 -0500

    añadir CONTACT

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,38 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView 
+from django.http import HttpResponse
+from django.views.generic import TemplateView
 
-class HomePageView(TemplateView):  
+# Create your views here.
+class HomePageView(TemplateView):
     template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "About us - Online Store",
+            "subtitle": "About us",
+            "description": "This is an about page ...",
+            "author": "Developed by: Your Name",
+        })
+        
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact Us - Online Store",
+            "subtitle": "Get in Touch",
+            "email": "info@onlinestore.com",
+            "address": "123 Main Street, Suite 100<br>New York, NY 10001<br>United States",
+            "phone": "+1 (555) 123-4567",
+        })
+        
+        return context
